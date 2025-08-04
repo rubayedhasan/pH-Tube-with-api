abf417f506d885e2cd975ed139e124d91a84925f
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-08-05 00:30:47 +0600

    fetching all data for videos and display them as a card dynamically

--- js/fetching.js
+++ js/fetching.js
@@ -21,5 +21,48 @@ const displayCategoriesAsBtn = (categories) => {
   });
 };
 
+// function:: fetching all data for videos from server
+const loadAllVideos = async () => {
+  const responseForVideos = await fetch(
+    "https://openapi.programming-hero.com/api/phero-tube/videos"
+  );
+  const dataOfVideos = await responseForVideos.json();
+
+  displayAllVideos(dataOfVideos.videos);
+};
+
+// function:: display all video cards
+const displayAllVideos = (videos) => {
+  const videoCardsContainer = document.querySelector("#video-cards-container");
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.classList = "card w-full";
+    videoCard.innerHTML = `
+    <figure>
+    <img
+      src="${video?.thumbnail}"
+      alt="${video?.title}"
+      class="object-cover h-[200px] w-full rounded" />
+  </figure>
+  <div class="flex items-start gap-5 pt-5">
+    <figure>
+    <img
+      src="${video?.authors[0]?.profile_picture}"
+      alt="${video?.authors[0]?.profile_name}"
+      class="w-10 h-10 rounded-full" />
+    </figure>
+    <div class="space-y-2">
+    <h5 class="font-bold text-[#171717] leading-6">${video?.title}</h5>
+    <p class="text-sm text-[#171717B3]">${video?.authors[0]?.profile_name}</p>
+    <p class="text-sm text-[#171717B3]">${video?.others?.views}</p>
+    </div>
+  </div>
+    `;
+
+    videoCardsContainer.appendChild(videoCard);
+  });
+};
+
 // calling the function globally
 loadCategories();
+loadAllVideos();
